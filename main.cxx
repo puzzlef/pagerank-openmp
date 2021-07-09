e49f3963f3e459f8551e6884e2ddd1a8e1ae9e57
Subhajit Sahu <wolfram77@gmail.com>
2021-07-09 15:04:33 +0530

    :skunk: w/ csv

--- main.cxx
+++ main.cxx
@@ -1,3 +1,5 @@
+#include <vector>
+#include <string>
 #include <cstdio>
 #include <iostream>
 #include "src/main.hxx"
@@ -8,31 +10,28 @@ using namespace std;
 
 
 template <class G, class H>
-void runPagerank(const G& x, const H& xt, bool show) {
-  int repeat = 5;
+void runPagerank(const G& x, const H& xt, int repeat) {
   vector<float> *init = nullptr;
 
   // Find pagerank using a single thread.
   auto a1 = pagerankSeq(xt, init, {repeat});
   auto e1 = absError(a1.ranks, a1.ranks);
   printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankSeq\n", a1.time, a1.iterations, e1);
-  if (show) println(a1.ranks);
 
   // Find pagerank accelerated using OpenMP.
   auto a2 = pagerankOmp(xt, init, {repeat});
   auto e2 = absError(a2.ranks, a1.ranks);
   printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankOmp\n", a2.time, a2.iterations, e2);
-  if (show) println(a2.ranks);
 }
 
 
 int main(int argc, char **argv) {
   char *file = argv[1];
-  bool  show = argc > 2;
+  int repeat = argc>2? stoi(argv[2]) : 5;
   printf("Loading graph %s ...\n", file);
   auto x  = readMtx(file); println(x);
   auto xt = transposeWithDegree(x); print(xt); printf(" (transposeWithDegree)\n");
-  runPagerank(x, xt, show);
+  runPagerank(x, xt, repeat);
   printf("\n");
   return 0;
 }
